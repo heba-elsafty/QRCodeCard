4422f14c98bb791be51f951eda5d6824cc01fcd1
heba-elsafty <heba.m.elsaft@gmail.com>
2024-08-14 17:51:58 +0300

    create 3 files, update 5 files, delete 3 files and rename 2 files

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,45 @@
 import React from "react";
 import QrCard from "./components/QrCode";
 import "./styles/global.scss";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
-    <div className="app">
-      <main>
-        <QrCard
-          title={"Improve your front-end skills by building projects"}
-          description={
-            "Scan the QR code to visit Frontend Mentor and take your coding skills to the next level"
-          }
-        />
-      </main>
+    <Router basename={process.env.PUBLIC_URL} >
+      <div className="app">
+        <main>
+          <QrCard
+            title={"Improve your front-end skills by building projects"}
+            description={
+              "Scan the QR code to visit Frontend Mentor and take your coding skills to the next level"
+            }
+          />
+        </main>
 
-      <footer>
-        <p className="text-sm">
-          <strong>Challenge by: </strong>
-          <a
-            href="https://www.frontendmentor.io"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Frontend Mentor.
-          </a>
-        </p>
-        <p className="text-sm">
-          <strong> Coded by: </strong>
-          <a
-            href="https://github.com/heba-elsafty"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Heba Elsafty
-          </a>
-        </p>
-      </footer>
-    </div>
+        <footer>
+          <p className="text-sm">
+            <strong>Challenge by: </strong>
+            <a
+              href="https://www.frontendmentor.io"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Frontend Mentor.
+            </a>
+          </p>
+          <p className="text-sm">
+            <strong> Coded by: </strong>
+            <a
+              href="https://github.com/heba-elsafty"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Heba Elsafty
+            </a>
+          </p>
+        </footer>
+      </div>
+    </Router>
   );
 };
 
