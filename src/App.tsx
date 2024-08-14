b9edf97e52282f6b071f82d38dcbc8a7777055cc
heba-elsafty <heba.m.elsaft@gmail.com>
2024-08-14 16:13:40 +0300

    ✨ feat: Add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,38 @@ import "./styles/global.scss";
 
 const App: React.FC = () => {
   return (
-    <div  className="app">
-      <QrCard
-        title={"Improve your front-end skills by building projects"}
-        description={"Scan the QR code to visit Frontend Mentor and take your coding skills to the next level"}
-      />
+    <div className="app">
+      <main>
+        <QrCard
+          title={"Improve your front-end skills by building projects"}
+          description={
+            "Scan the QR code to visit Frontend Mentor and take your coding skills to the next level"
+          }
+        />
+      </main>
+
+      <footer>
+        <p className="text-sm">
+          <strong>Challenge by: </strong>
+          <a
+            href="https://www.frontendmentor.io"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Frontend Mentor.
+          </a>
+        </p>
+        <p className="text-sm">
+          <strong> Coded by: </strong>
+          <a
+            href="https://github.com/heba-elsafty"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Heba Elsafty
+          </a>
+        </p>
+      </footer>
     </div>
   );
 };
