8a8db1f9224b3252ce70cb2356863dac4ddb3d95
heba-elsafty <heba.m.elsaft@gmail.com>
2024-08-14 15:34:01 +0300

    Add mixins & var

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 // src/App.tsx
 import React from "react";
 import QrCard from "./components/QrCode";
+import "./styles/global.scss";
 
 const App: React.FC = () => {
   return (
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", padding: "1rem" }}>
+    <div  className="app">
       <QrCard
         title={"Improve your front-end skills by building projects"}
         description={"Scan the QR code to visit Frontend Mentor and take your coding skills to the next level"}
