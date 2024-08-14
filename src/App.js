4a25ac32e8ae3018ac0e0ec837d5b6217320cbf2
heba-elsafty <heba.m.elsaft@gmail.com>
2024-08-14 17:16:35 +0300

    Updates

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import QrCard from "./components/QrCode";
-import "./styles/global.scss";
-const App = () => {
-    return (_jsxs("div", { className: "app", children: [_jsx("main", { children: _jsx(QrCard, { title: "Improve your front-end skills by building projects", description: "Scan the QR code to visit Frontend Mentor and take your coding skills to the next level" }) }), _jsxs("footer", { children: [_jsxs("p", { className: "text-sm", children: [_jsx("strong", { children: "Challenge by: " }), _jsx("a", { href: "https://www.frontendmentor.io", target: "_blank", rel: "noopener noreferrer", children: "Frontend Mentor." })] }), _jsxs("p", { className: "text-sm", children: [_jsx("strong", { children: " Coded by: " }), _jsx("a", { href: "https://github.com/heba-elsafty", target: "_blank", rel: "noopener noreferrer", children: "Heba Elsafty" })] })] })] }));
-};
-export default App;
